a907d5eac4d82e2ec5fed5aa9e337176add57f3a
Deltacripto <ecobossbolsas@gmail.com>
2025-06-25 12:27:38 +0200

    bot.py

--- bot.py
+++ bot.py
@@ -385,6 +385,6 @@ def identificar_activo_en(ticker):
 # ------------------------------------------------------------------------------
 # 8. EJECUCIÓN DEL SERVIDOR FLASK
 # ------------------------------------------------------------------------------
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 1000))
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
